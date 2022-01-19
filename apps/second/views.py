766c61ede833bacf5c08b2de561646336eabe7e9
ImpressionF <1002388896@qq.com>
2022-01-19 11:43:15 +0800

    second.views update

--- apps/second/views.py
+++ apps/second/views.py
@@ -7,6 +7,6 @@ from django.shortcuts import render
 def goSecond(requeset):
     # content = 'aa'
     # return render(requeset, 'goSecond.html', content)
-    name = 'fyj'
+    name = 'bro'
     nums = [1, 2, 3, 4]
     return render(requeset, 'goSecond.html', {'name': name, 'nums': nums});
